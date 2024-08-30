f727f02dce863d720b25837cf6da2a3ef31845c5
Madhav Shri <madhavshri5@gmail.com>
2024-08-30 16:09:15 +0530

    Initial commit for her

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,61 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { interval, Observable } from "rxjs";
+import { map, shareReplay } from "rxjs/operators";
+import { timeComponents } from './Interfaces/timeComponent.interface';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'DeepikaGunasekar';
+  title = 'Deepika Gunasekar';
+  public timeLeft$: Observable<timeComponents>;
+  //constructor(){this.timeLeft$=}
+  constructor() {
+    this.timeLeft$ = interval(1000).pipe(
+      map(x => this.calcDateDiff()),
+      shareReplay(1)
+    );
+  }
+
+
+
+   calcDateDiff(endDay: Date = new Date(2025, 1, 13)): timeComponents {
+    const dDay = endDay.valueOf();
+
+
+    const milliSecondsInASecond = 1000;
+    const hoursInADay = 24;
+    const minutesInAnHour = 60;
+    const secondsInAMinute = 60;
+
+    const timeDifference = dDay - Date.now();
+
+    const daysToDday = Math.floor(
+      timeDifference /
+        (milliSecondsInASecond * minutesInAnHour * secondsInAMinute * hoursInADay)
+    );
+
+    const hoursToDday = Math.floor(
+      (timeDifference /
+        (milliSecondsInASecond * minutesInAnHour * secondsInAMinute)) %
+        hoursInADay
+    );
+
+    const minutesToDday = Math.floor(
+      (timeDifference / (milliSecondsInASecond * minutesInAnHour)) %
+        secondsInAMinute
+    );
+
+    const secondsToDday =
+      Math.floor(timeDifference / milliSecondsInASecond) % secondsInAMinute;
+
+    return { secondsToDday, minutesToDday, hoursToDday, daysToDday };
+  }
+
 }
